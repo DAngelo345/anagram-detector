4301a9951da1109e0e7aafe1aed9747b538c9feb
DAngelo Thomas <dangelo1thomas@gmail.com>
2020-11-06 17:45:16 -0500

    commit tree 421d216f3c02b0eb894a99c42a56aa3d360ba0ca

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+
+require "pry"
+class Anagram
+
+    attr_accessor :name
+
+
+    def initialize(name)
+        @name = name
+    end
+
+  def match(word_array)
+    array = []
+    word_array.each do |word|
+        if word.length === name.length
+           if word.split(//).sort === name.split(//).sort
+            array << word
+           end
+        end
+    end
+return array 
+  end
+end
